c1d117bf151312a3a37183f0ee1efc8579281eb7
prgrmz07 <prgrmz07@163.com>
2023-12-01 17:13:19 +0800

    dir_util.sh

---
+++ dir_util.sh
@@ -0,0 +1,16 @@
+#创建目录、设置该目录主人为 当前主组/当前用户 、进入该目录
+#使用举例: 
+#createDir_CurUsrOwn_EnterIt /app 
+function createDir_CurUsrOwn_EnterIt(){
+    
+    
+    #若函数参数少于1个，则退出（退出码为21）
+    [ $# -lt 1 ] && return 21
+
+    dire=$1
+    mainGroup=$(id -gn) && \
+    $username=$(whoami) && \
+    sudo mkdir $dire && \
+    sudo chown -R $mainGroup.$username $dire && \
+    cd $dire
+}
