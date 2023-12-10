2fff7e0c31d26afcfa176efb678ffdd32a64ef62
prgrmz01@163.com <prgrmz01@163.com>
2023-12-10 17:15:46 +0800

    mvFile_AppendCurAbsTime： 全路径文件 重命名（ 加 当前绝对时间后缀）

--- dir_util.sh
+++ dir_util.sh
@@ -14,3 +14,17 @@ function createDir_CurUsrOwn_EnterIt(){
     sudo chown -R $mainGroup.$username $dire && \
     cd $dire
 }
+
+#全路径文件 重命名： 加 当前绝对时间后缀
+#用法举例: mvFile_AppendCurAbsTime /crk/xxx.txt
+#则 文件/crk/xxx.txt 被重命名为 比如 /crk/xxx.txt-20231210132251_1702185771_452355256
+function mvFile_AppendCurAbsTime(){
+        
+    #若函数参数少于1个，则退出（退出码为21）
+    [ $# -lt 1 ] && return 22
+
+    fileName=$1 && \
+    # 比如 fileName==/crk/xxx.txt && \
+    UniqueId="$fileName-$(date +'%Y%m%d%H%M%S_%s_%N')" && \
+    [ -f $fileName ] && mv $fileName "$fileName_$UniqueId"
+}
