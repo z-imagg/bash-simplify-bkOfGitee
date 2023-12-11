df30fdc4e97e7dba949edc163d2549df602745a8
prgrmz01@163.com <prgrmz01@163.com>
2023-12-11 14:08:38 +0800

    mvFile_AppendCurAbsTime_multi; fix mvFile_AppendCurAbsTime

--- dir_util.sh
+++ dir_util.sh
@@ -26,5 +26,21 @@ function mvFile_AppendCurAbsTime(){
     fileName=$1 && \
     # 比如 fileName==/crk/xxx.txt && \
     UniqueId="$fileName-$(date +'%Y%m%d%H%M%S_%s_%N')" && \
-    [ -f $fileName ] && mv $fileName "$fileName_$UniqueId"
+    [ -f $fileName ] && mv $fileName "$UniqueId"
+    #fix bug
+}
+
+function mvFile_AppendCurAbsTime_multi(){
+        
+    #若函数参数少于1个，则退出（退出码为21）
+    [ $# -lt 1 ] && return 22
+
+
+    UniqueDateTime="$(date +'%Y%m%d%H%M%S_%s_%N')" && \
+
+    for fileNameK in "$@"; do
+        UniqueId="${fileNameK}-${UniqueDateTime}" && \
+        [ -f $fileNameK ] && mv $fileNameK "$UniqueId"
+    done
+
 }
