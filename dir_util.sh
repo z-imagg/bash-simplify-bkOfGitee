ebde7a23c479702faa6f766ff76799af94bee9f3
prgrmz01@163.com <prgrmz01@163.com>
2023-12-19 22:11:11 +0800

    miniconda3install

--- dir_util.sh
+++ dir_util.sh
@@ -102,14 +102,30 @@ function get_out_en_dbg(){
     # echo $_out_en_dbg
 }
 
+function miniconda3install(){
+(echo "安装miniconda3..." && \
+cd /tmp/ && \
+cat << 'EOF' > Miniconda3-py310_23.10.0-1-Linux-x86_64.sh.md5sum.txt
+cefadd1cacd8e5b9a74b404df1f11016  Miniconda3-py310_23.10.0-1-Linux-x86_64.sh
+EOF
+
+{ md5sum --check Miniconda3-py310_23.10.0-1-Linux-x86_64.sh.md5sum.txt ||
+ wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py310_23.10.0-1-Linux-x86_64.sh ;} && \
+
+hm=/app/miniconda3 && \
+sudo mkdir -p $hm && \
+sudo chown -R $(id -gn).$(whoami) $hm && \
+bash Miniconda3-py310_23.10.0-1-Linux-x86_64.sh -b -u -p $hm
+)
+}
+
 function miniconda3Activate(){
 get_out_en_dbg && \
 # echo "$_out_en_dbg,【$_out_dbg】" && \
 
 #miniconda activate 不要开调试
 CondaActvF=/app/miniconda3/bin/activate && \
-{ [ -f  $CondaActvF ] || \
-  { echo "错误,无miniconda3,请手工安装miniconda3形如${CondaActvF}，退出码51" && exit 51 ;} ;} && \
+{ [ -f  $CondaActvF ]  || miniconda3install   ;} && \
 set +x && source $CondaActvF
 
 #恢复可能的调试
