1fd21809a2884aa4ecc1e3e68fdc87207619275b
prgrmz01@163.com <prgrmz01@163.com>
2023-12-19 23:27:00 +0800

    cmakeInstall

--- dir_util.sh
+++ dir_util.sh
@@ -135,6 +135,7 @@ set +x && source $CondaActvF
 
 function cmakeInstall(){
     
-{ cmake --version 1&>2 /dev/null || sudo apt install cmake  build-essential -y ;}
+{ cmake --version 1>/dev/null 2>/dev/null || sudo apt install cmake    -y ;} && \
+{ g++ --version   1>/dev/null 2>/dev/null || sudo apt install build-essential -y ;}
 
 }
