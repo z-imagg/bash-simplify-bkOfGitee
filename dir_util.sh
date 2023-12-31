2540bf7886aaadc95b1ae56a20ce4ed760c2b7e3
prgrmz07 <prgrmz07@163.com>
2023-12-31 11:56:10 +0800

    fileModifiedInNSeconds: 给定文件的最后修改时刻是否在当前时刻的N秒内

--- dir_util.sh
+++ dir_util.sh
@@ -162,3 +162,21 @@ lnkSrc=$1  &&  lnkDest=$2 && \
 ln -s $lnkSrc $lnkDest ;}
 
 }
+
+
+#给定文件的最后修改时刻是否在当前时刻的N秒内
+function fileModifiedInNSeconds(){
+# fileModifiedInNSeconds "/bal/clang-add-funcIdAsm/build/lib/libCTk.so" "5*60"
+
+#若函数参数少于2个，则退出（退出码为14）
+[ $# -lt 2 ] && return 14
+filePath=$1  &&  limitSecondsExpr=$2 && \
+{ [ -e $filePath ] || return 31 ;} && \
+limitSeconds=$(($limitSecondsExpr)) && \
+nowSeconds=$(date +%s) && \
+fileEndModifySeconds=$(stat -c %Y  $filePath ) && \
+deltaSeconds=$(( $nowSeconds -  $fileEndModifySeconds  )) && \
+{ [  $deltaSeconds  -le $limitSeconds ] || return 1 ;} && \
+return 0
+
+}
