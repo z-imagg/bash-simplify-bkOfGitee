96a9cf3027f3ef279f5b32310935efcad2b30d98
prgrmz07 <prgrmz07@163.com>
2024-03-19 15:36:34 +0800

    更新 cur_script_full_path.sh

--- cur_script_full_path.sh
+++ cur_script_full_path.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-#source me.sh 或 bash me.sh 均能获取当前脚本完整路径的写法（且引入的变量都是必须要的），完全可以取代dir_util.sh 中相关函数 getCurScriptDirName、 getCurScriptDirByConcat 
+#此行必须有，否则alias x后，x不会被当成命令 （会说找不到x）
+shopt -s expand_aliases
 
+#source me.sh 或 bash me.sh 均能获取当前脚本完整路径的写法（且引入的变量都是必须要的），完全可以取代dir_util.sh 中相关函数 getCurScriptDirName、 getCurScriptDirByConcat 
 alias getCurScriptFullPath='f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f) '
 
 #调用例子:
@@ -9,13 +11,3 @@ alias getCurScriptFullPath='f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f
 #getCurScriptFullPath
 #echo $d,$f  #f为当前脚本完整路径， d为当前脚本所在目录的绝对路径
 
-
-
-#如果你想直接在你的脚本中写出来，并理解使用，而不是引用此文件，则可以如下这样写
-# # source <(echo 'alias zls="ls"')
-# #上一行 中 echo替换为cat，则得到如下行, 好处是 cat不用处理引号转义问题，而echo则必须处理引号转义问题
-# source <(cat << 'EOF'  
-# alias getCurScriptFullPath='f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f) '
-# EOF
-# )
-# getCurScriptFullPath
