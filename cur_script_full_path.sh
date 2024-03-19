3c9a79a7172dd220381f78354c677afd6cacc7aa
prgrmz07 <prgrmz07@163.com>
2024-03-19 15:26:12 +0800

    更新 cur_script_full_path.sh

--- cur_script_full_path.sh
+++ cur_script_full_path.sh
@@ -8,3 +8,14 @@ alias getCurScriptFullPath='f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f
 #source cur_script_full_path.sh
 #getCurScriptFullPath
 #echo $d,$f  #f为当前脚本完整路径， d为当前脚本所在目录的绝对路径
+
+
+
+#如果你想直接在你的脚本中写出来，并理解使用，而不是引用此文件，则可以如下这样写
+# # source <(echo 'alias zls="ls"')
+# #上一行 中 echo替换为cat，则得到如下行, 好处是 cat不用处理引号转义问题，而echo则必须处理引号转义问题
+# source <(cat << 'EOF'  
+# alias getCurScriptFullPath='f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f) '
+# EOF
+# )
+# getCurScriptFullPath
