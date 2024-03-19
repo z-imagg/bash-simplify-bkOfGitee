dfc9009e0b6160862683916a10774929ce5c5277
prgrmz07 <prgrmz07@163.com>
2024-03-19 14:49:52 +0800

    source me.sh 或 bash me.sh 均能获取当前脚本完整路径的写法（且引入的变量都是必须要的），，完全可以取代dir_util.sh 中相关函数

---
+++ cur_script_full_path.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#source me.sh 或 bash me.sh 均能获取当前脚本完整路径的写法（且引入的变量都是必须要的），完全可以取代dir_util.sh 中相关函数 getCurScriptDirName、 getCurScriptDirByConcat 
+
+alias getCurScriptFullPath='f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f) '
+
+#调用例子:
+#source cur_script_full_path.sh
+#getCurScriptFullPath
+#echo $d,$f  #f为当前脚本完整路径， d为当前脚本所在目录的绝对路径
