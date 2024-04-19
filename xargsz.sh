aa6e9705e0ac0228932e47fd69d6544c19de264f
prgrmz07 <prgrmz07@163.com>
2024-04-19 13:13:35 +0800

    xargsz(等效于xargs的自定义普通bash函数). eval内function使得变量名是局部的

--- xargsz.sh
+++ xargsz.sh
@@ -18,11 +18,15 @@ fi
 # echo "参数列表作为一个字符串\$*=【$*】"
 # echo "（参数列表作为字符串数组,使用请看变量arg_arr）; ${arg_arr[0]}=xxx; \$@=【$@】"
 local busyFuncName="$1"
-export -f $busyFuncName
 
 # 循环读取输入
 while IFS= read -r line; do
-    bash -c "Ln=$line $busyFuncName"
+#eval内再包裹函数的目的是为了让变量Ln以local修饰，从而名字Ln只是一个局部名字，对外层名字没有影响。
+    eval """
+function __func__wrap() { local Ln=$line ; $busyFuncName ;} ;
+__func__wrap ; 
+unset __func__wrap; 
+"""
 done
 }
 
