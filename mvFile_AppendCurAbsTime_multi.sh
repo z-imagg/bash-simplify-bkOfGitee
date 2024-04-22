cd3d79e18550c5cae3372d0aa1e7d22147a7e746
prgrmz07 <prgrmz07@163.com>
2024-04-22 21:29:19 +0800

    添加 mvFile_AppendCurAbsTime_multi.sh

---
+++ mvFile_AppendCurAbsTime_multi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source <(curl http://giteaz:3000/bal/bash-simplify/raw/branch/release/mvFLsByAbsTm.sh)
+
+
+#全路径文件 重命名： 加 当前绝对时间后缀
+#用法举例: mvFile_AppendCurAbsTime /bal/xxx.txt
+#则 文件/bal/xxx.txt 被重命名为 比如 /bal/xxx.txt-20231210132251_1702185771_452355256
+function mvFile_AppendCurAbsTime_multi(){
+mvFLsByAbsTm $*
+}
