ca46064c27759ae057fb35dedae01156012063f5
prgrmz07 <prgrmz07@163.com>
2024-04-22 21:49:27 +0800

    更新 cdCurScriptDir.sh

--- cdCurScriptDir.sh
+++ cdCurScriptDir.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#若仓库"/app/bash-simplify/"不处在relese分支，则退出
+source <(curl http://giteaz:3000/bal/bash-simplify/raw/branch/release/repo_branch_eq_release.sh)
+repo_branch_eq_release || exit $?
+
 #去此脚本所在目录
 function cdCurScriptDir(){
 #去此脚本所在目录
