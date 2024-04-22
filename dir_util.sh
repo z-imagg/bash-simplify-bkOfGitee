83793b2e382e339ffe2e69202503226439f759ad
prgrmz07 <prgrmz07@163.com>
2024-04-22 21:37:22 +0800

    更新 dir_util.sh

--- dir_util.sh
+++ dir_util.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-source <(curl http://giteaz:3000/bal/bash-simplify/raw/branch/release/cdCurScriptDir.sh)
+Hm="/app/bash-simplify/"
+[[ -f $Hm/.git/config ]] && echo ok
 
 
-source getCurScriptDirName.sh
-source mvFile_AppendCurAbsTime.sh
-source mvFile_AppendCurAbsTime_multi.sh
-source gitCko_tagBrc_asstCmtId.sh
-source get_out_en_dbg.sh
-source miniconda3install.sh #tsinghua_pypi_src miniconda3Activate
-source cmakeInstall.sh
-source makLnk.sh
-source fileModifiedInNSeconds.sh
+source $Hm/cdCurScriptDir.sh
+
+source $Hm/getCurScriptDirName.sh
+source $Hm/mvFile_AppendCurAbsTime.sh
+source $Hm/mvFile_AppendCurAbsTime_multi.sh
+source $Hm/gitCko_tagBrc_asstCmtId.sh
+source $Hm/get_out_en_dbg.sh
+source $Hm/miniconda3install.sh #tsinghua_pypi_src miniconda3Activate
+source $Hm/cmakeInstall.sh
+source $Hm/makLnk.sh
+source $Hm/fileModifiedInNSeconds.sh
