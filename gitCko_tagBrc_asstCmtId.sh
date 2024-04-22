a761a775a0f437655482cce4c0d8977b28f7ac72
prgrmz07 <prgrmz07@163.com>
2024-04-22 21:52:02 +0800

    更新 gitCko_tagBrc_asstCmtId.sh

--- gitCko_tagBrc_asstCmtId.sh
+++ gitCko_tagBrc_asstCmtId.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-source <(curl http://giteaz:3000/bal/bash-simplify/raw/branch/release/gitCko_tagBrc_assertCmtId.sh)
+#若仓库"/app/bash-simplify/"不处在relese分支，则退出
+source <(curl http://giteaz:3000/bal/bash-simplify/raw/branch/release/repo_branch_eq_release.sh)
+repo_branch_eq_release || exit $?
+Hm="/app/bash-simplify/"
 
+source $Hm/gitCko_tagBrc_assertCmtId.sh
 
 #gitCko_tagBrc_asstCmtId: gitCheckout_tagOrBranch_assertCmtId
 #调用举例 : gitCko_tagBrc_asstCmtId GitDir tagOrBranch CmtId
