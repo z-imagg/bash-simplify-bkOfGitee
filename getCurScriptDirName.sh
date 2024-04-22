95e4b898f0c19ff2050e9bdb16b67f2007433f1e
prgrmz07 <prgrmz07@163.com>
2024-04-22 21:22:11 +0800

    添加 getCurScriptDirName.sh

---
+++ getCurScriptDirName.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#与此类似  http://g:3000/bal/bash-simplify/_edit/release/cdCurScriptDir.sh
+
+#去此脚本所在目录
+function cdCurScriptDir(){
+#去此脚本所在目录
+declare -r CurScriptNm=$(readlink -f ${BASH_SOURCE[0]})  ; declare -r CurScriptDir=$(dirname $f)
+# cd $CurScriptDir
+}
