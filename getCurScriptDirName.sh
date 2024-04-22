7a38947b5e74e786f6c2d033a7a87b56fc518afc
prgrmz07 <prgrmz07@163.com>
2024-04-22 21:49:53 +0800

    更新 getCurScriptDirName.sh

--- getCurScriptDirName.sh
+++ getCurScriptDirName.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#若仓库"/app/bash-simplify/"不处在relese分支，则退出
+source <(curl http://giteaz:3000/bal/bash-simplify/raw/branch/release/repo_branch_eq_release.sh)
+repo_branch_eq_release || exit $?
+
+
 #与此类似  http://g:3000/bal/bash-simplify/_edit/release/cdCurScriptDir.sh
 
 #去此脚本所在目录
