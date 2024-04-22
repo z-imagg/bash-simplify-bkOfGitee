a652d68f47997ae211bb77ceee13cea6cbd6139b
prgrmz07 <prgrmz07@163.com>
2024-04-22 21:43:28 +0800

    添加 repo_branch_eq.sh

---
+++ repo_branch_eq.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function repo_branch_eq() {
+local Hm="/app/bash-simplify/"
+local GitHm="$Hm/.git/"
+[[ -f $GitHm/config ]] && echo ok
+
+local branch=$(git --git-dir=$GitHm rev-parse --abbrev-ref HEAD)
+
+# local errCode_branchBad=21
+# local errMsg_branchBad="$GitHm 's branch is not release, exit code $errCode_branchBad"
+[[ "_$branch" == "_release" ]]
+# || { echo $errMsg_branchBad && exit  $errCode_branchBad ;}
+
+}
