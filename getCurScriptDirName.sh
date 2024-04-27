0c9814f5c0ac4823ac74b1d67ab1db6c9aaa935e
prgrmz07 <prgrmz07@163.com>
2024-04-28 00:42:00 +0800

    getCurScriptDirName

---
+++ getCurScriptDirName.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#去此脚本所在目录
+function getCurScriptDirName(){
+#'caller 0' == '直接调用者所在行号 直接调用者函数名 直接调用者函数所在源文件'
+local callerInfo__lnNum_func_file=$(caller 0)
+#转为数组
+local str_arr=(${callerInfo__lnNum_func_file})
+#直接调用者所在行号
+local caller_lnNum=${str_arr[0]}
+#直接调用者函数名
+local caller_func=${str_arr[1]}
+#直接调用者函数所在源文件
+local caller_file=${str_arr[2]}
+
+local dirPth=$(dirname $caller_file)
+
+#返回变量 __fn 、 __dire
+__fn=$caller_file
+__dire=$dirPth
+
+}
+
+
+#运行举例
+# source /app/bash-simplify/cdCurScriptDir.sh
+# getCurScriptDirName
