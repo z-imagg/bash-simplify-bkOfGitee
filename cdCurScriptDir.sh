346fedfd57b238aeeece208766066019d19caece
prgrmz07 <prgrmz07@163.com>
2024-04-27 23:33:48 +0800

    删除垃圾实现： cdCurScriptDir 、 getCurScriptDirName

--- cdCurScriptDir.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/getCurScriptDirName.sh)
-
-#去此脚本所在目录
-function cdCurScriptDir(){
-    getCurScriptDirName && cd $__dire
-}
