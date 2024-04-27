2ddb15ba772566978acea6835ea3fbc29ddb823a
prgrmz07 <prgrmz07@163.com>
2024-04-27 22:24:52 +0800

    git代理设置 、 git代理取消

---
+++ gitproxy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#【描述】  git代理设置 、 git代理取消
+#【依赖】   
+#【术语】 
+#【备注】  
+
+#通过ip引入
+#   若设置本地域名失败，则退出代码27.  一般放在脚本靠前, 因为这句正常执行后 本地域名giteaz才能用
+{ source <(curl --silent http://10.0.4.9:3000/bal/bash-simplify/raw/branch/release/local_domain_set.sh)  && local_domain_set ;} || exit 27
+
+
+function gitproxy_set() {
+git config --global http.proxy socks5://westgw:7890 ; 
+git config --global https.proxy socks5://westgw:7890  ; 
+}
+
+function gitproxy_unset(){
+git config --global --unset http.proxy ; 
+git config --global --unset https.proxy  
+}
+
+#用法举例: 
+# source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/gitproxy.sh)
+# git设置代理, 执行git命令, git取消代理
+# gitproxy_set ; git clone https://github.com/x.git /x;  git --git-dir=/x/.git/  submodule    update --recursive --init;   gitproxy_unset
