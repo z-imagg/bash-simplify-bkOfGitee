58c9a86949dbcea30c821e6e29f94e84e200f478
prgrmz07 <prgrmz07@163.com>
2024-04-27 12:38:27 +0800

    git忽略 文件可执行权限变更

---
+++ git_ignore_filemode.sh
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env bash
+
+#【描述】  
+#【依赖】   
+#【术语】 
+#【备注】  
+
+# git忽略 文件可执行权限变更
+function git_ignore_filemode() {
+    (  git config --unset-all core.filemode  ; git config core.filemode false ; true ;)
+}
