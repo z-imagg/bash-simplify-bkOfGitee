c993c9e6b3ea0236eb7d82c8cb8987a0c6ab0490
prgrmz07 <prgrmz07@163.com>
2024-04-28 15:48:54 +0800

    fix git_clone_branchOrTag_toDir

--- git_clone_branchOrTag_toDir.sh
+++ git_clone_branchOrTag_toDir.sh
@@ -14,18 +14,23 @@ source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/g
 
 # git克隆仓库的给定分支或标签到给定目录
 function git_clone_branchOrTag_toDir() {
+    local ExitCode_Ok=0
 
     #  若参数个数不为3个 ，则返回错误
     echo 3 | argCntEqN $* || return $?
 
     #git仓库Url
-    repoUrl=$1
+    local repoUrl=$1
     #初始分支名称
-    initBrch=$2
+    local initBrch=$2
     #git仓库存放目录
-    repoDir=$3
+    local repoDir=$3
+    local repoCfgF="$repoDir/.git/config"
 
-    #若git仓库存放目录存在，则返回错误（退出码为34）
+    #若已经是一个git仓库，则返回正常（退出码为0）
+    [[ -f $repoCfgF ]] && return $ExitCode_Ok
+
+    #否则 即目录存在 但不是一个git仓库，则返回错误（退出码为34）
     [[ -e $repoDir ]] && return 34
 
 git clone -b $initBrch $repoUrl  $repoDir && \
