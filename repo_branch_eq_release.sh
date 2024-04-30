cce4e232e72e1003015b72c67c4e2f90cf1eff4a
prgrmz07 <prgrmz07@163.com>
2024-04-30 15:47:16 +0800

    重写repo_branch_eq_release: bash-simplify的标签从tag/release改为tag_release

--- repo_branch_eq_release.sh
+++ repo_branch_eq_release.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/git__chkDir__get__repoDir__arg_gitDir.sh)
+
 function repo_branch_eq_release() {
-local errCode_notGitRepo=12
 local errCode_branchNotRelease=13
 local Ok=0
 
 local Hm="/app/bash-simplify/"
-local GitHm="$Hm/.git/"
-[[ -f $GitHm/config ]] || return $errCode_notGitRepo
 
-local branch=$(git --git-dir=$GitHm rev-parse --abbrev-ref HEAD)
+#若 该目录不是git仓库， 则返回错误
+# git 检查仓库目录 、 获取仓库目录 、 获取git目录参数 , 返回变量为 repoDir 、 arg_gitDir
+git__chkDir__get__repoDir__arg_gitDir $Hm || return $?
 
-[[ "_$branch" == "_release" ]] || return $errCode_branchNotRelease
+#HEAD是否在tag_release标签上
+local HeadIn__tag_release=false; [[ $(git $arg_gitDir  tag   --points-at HEAD  --list "tag_release" | wc -l) == 1 ]] && HeadIn__tag_release=true
 
+#若否定, 则返回错误
+$HeadIn__tag_release || return $errCode_branchNotRelease
+
+#否则即肯定，则返回正常
 return  $Ok
 }
