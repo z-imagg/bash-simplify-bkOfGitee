43fe7e84c8969e2f3b19da868602e68bdc75c53b
prgrmz07 <prgrmz07@163.com>
2024-04-30 16:30:49 +0800

    导入在标签tag_release上的给定脚本

--- _importFn.sh
+++ _importFn.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-#【描述】  导入在标签tag_release上的给定函数
+#【描述】  导入在标签tag_release上的给定脚本
 #【依赖】   
-#【术语】 Fn==function
+#【术语】 scriptFN==scriptFileName
 #【备注】  
 
-function _importFn__tag_release() {
+function _importFn() {
+local  tagName="tag_release"
 
 #若函数参数不为1个 ， 则返回错误（退出码为23）
 [ $# -eq 1 ] || return 23
 
-#函数名functionName==脚本文件名scriptFileName
-local scriptFileName=$1
+local scriptFN=$1
 
-source <(curl --silent "http://giteaz:3000/bal/bash-simplify/raw/tag/tag_release/${scriptFileName}.sh")
+#若文件名不以'.sh'结尾, 则补上
+[[ $scriptFN = *.sh ]] || scriptFN="$scriptFN.sh"
+
+source <(curl --silent "http://giteaz:3000/bal/bash-simplify/raw/tag/${tagName}/${scriptFN}")
 #上一行带入实际值 后 举例如下:
 # source <(curl --silent "http://giteaz:3000/bal/bash-simplify/raw/tag/tag_release/git__chkDir__get__repoDir__arg_gitDir.sh")
-
 }
 
 #使用举例
-# source <(curl --silent "http://giteaz:3000/bal/bash-simplify/raw/tag/tag_release/_importFn__tag_release.sh")
-# _importFn__tag_release git__chkDir__get__repoDir__arg_gitDir
+# source <(curl --silent "http://giteaz:3000/bal/bash-simplify/raw/tag/tag_release/_importFn.sh")
+# _importFn git__chkDir__get__repoDir__arg_gitDir
+#  等同于
+# _importFn git__chkDir__get__repoDir__arg_gitDir.sh
 
