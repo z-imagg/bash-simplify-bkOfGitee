fb45b6c01d60970e1d276d5b0eb69d6b4cca5756
prgrmz07 <prgrmz07@163.com>
2024-04-30 15:15:01 +0800

    git_clone_branchOrTag_toDir.sh改为git_clone_Tag_toDir.sh

--- git_Clone_SwitchTag.sh
+++ git_Clone_SwitchTag.sh
@@ -7,13 +7,14 @@
 
 
 source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/git_reset.sh)
+source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/git_switch_to_remote_tag.sh)
 source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/git__chkDir__get__repoDir__arg_gitDir.sh)
 source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/argCntEqN.sh)
 source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/git_ignore_filemode_noCd.sh)
 
 
-# 若该目录不存在,则git克隆仓库的给定分支或标签到给定目录
-function git_clone_branchOrTag_toDir() {
+# git克隆仓库的给定标签到本地目录 或 切换本地仓库到给定标签
+function git_Clone_SwitchTag() {
     local ExitCode_Ok=0
 
     #  若参数个数不为3个 ，则返回错误
@@ -30,7 +31,7 @@ function git_clone_branchOrTag_toDir() {
 #若主机目录是合法git仓库，  
 git__chkDir__get__repoDir__arg_gitDir "$hostRepoDir" && \
 #则 切换到给定分支 
-{ git_switch_to_remote_branch "$hostRepoDir" "$initBrch" && \
+{ git_switch_to_remote_tag "$hostRepoDir" "$initBrch" && \
 # 子模块更新
 ( cd $repoDir && git  submodule    update --recursive --init ;) && \
 # 返回正常
@@ -49,6 +50,6 @@ git_ignore_filemode_noCd $repoDir
 
 # #用法举例:
 #  git克隆仓库的给定分支或标签到给定目录
-# source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/git_clone_branchOrTag_toDir.sh)
+# source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/git_Clone_SwitchTag.sh)
 #  克隆 https://github.com/qemu/qemu.git 的标签 v5.0.0 到 本地目录 /tmp/qemu/ 
-# git_clone_branchOrTag_toDir https://github.com/qemu/qemu.git v5.0.0 /tmp/qemu/ 
+# git_Clone_SwitchTag https://github.com/qemu/qemu.git v5.0.0 /tmp/qemu/ 
