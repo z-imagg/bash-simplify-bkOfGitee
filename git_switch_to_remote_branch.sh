8a0536fec2edbbafcc0e76605d7729cb15abbf95
prgrmz07 <prgrmz07@163.com>
2024-04-30 14:26:43 +0800

    git_switch_to_remote_branch：放弃检查本地分支，理由是本地分支千奇百怪

--- git_switch_to_remote_branch.sh
+++ git_switch_to_remote_branch.sh
@@ -14,7 +14,7 @@ source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/a
 #  核心命令举例 'git checkout -b linux-5.1.y --track origin/linux-5.1.y' 
 #   git_switch_to_remote_branch  /bal/linux-stable linux-5.1.y == 将git仓库'/bal/linux-stable'切换到远程分支 linux-5.1.y ， 并在该提交上建立本地分支linux-5.1.y
 function git_switch_to_remote_branch() {
-    local ExitCode_NoRemoteBranch=0
+    local ExitCode_NoRemoteBranch=31
 
     #  若函数参数不为2个 ， 则返回错误
     argCntEq2 $* || return $?
@@ -24,8 +24,9 @@ function git_switch_to_remote_branch() {
     git__chkDir__get__repoDir__arg_gitDir $* || return $?
     
     #本地分支名称
-    local brchLocal=$2
-    local remoteBranch="refs/heads/$brchLocal"
+    local branch=$2
+    local remoteBranch="refs/heads/$branch" # refs/heads为远程分支前缀
+    # local remoteTag="refs/tags/$tagName"  # refs/tags为远程分支前缀
     local ErrMsg_NoRemoteBranch="无该远程分支 [$remoteBranch], exit $ExitCode_NoRemoteBranch"
 
     #是否有对应的远程分支
@@ -35,9 +36,12 @@ function git_switch_to_remote_branch() {
     ( ! $hasRemoteBranch ) && { echo $ErrMsg_NoRemoteBranch ; return $ExitCode_NoRemoteBranch ;}
     
     #否则，重置工作树、强制删除该本地分支、检出该本地分支并跟踪该远程分支
-    git_reset $repoDir ;
-    git $arg_gitDir branch --delete --force "$brchLocal" ; 
-    git $arg_gitDir checkout -b  "$brchLocal"   --track "origin/$brchLocal"
+    # git reset 可能报错，忽略
+    git_reset $repoDir
+    # git 删除分支 可能报错，忽略
+    git $arg_gitDir branch --delete --force "$branch"
+    # 检出本地分支并跟踪远程分支
+    git $arg_gitDir checkout -b  "$branch"   --track "origin/$branch"
 
 
 
