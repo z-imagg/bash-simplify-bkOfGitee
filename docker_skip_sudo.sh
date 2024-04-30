93dde99717bf32f5dbf6f89dc65f32ce00111968
prgrmz07 <prgrmz07@163.com>
2024-04-30 23:33:10 +0800

    加入docker用户组后，需要新开ssh才生效

--- docker_skip_sudo.sh
+++ docker_skip_sudo.sh
@@ -15,7 +15,11 @@ mainGroup=$(id -gn) && \
 ( sudo groupadd docker || true ; \
 sudo gpasswd -a $mainGroup docker ; \
 sudo systemctl  restart docker || true ; \
-sudo systemctl  start docker || true ;)
+sudo systemctl  start docker || true ;) \
+#newgrp会启动新shell, 不适合脚本用
+# newgrp - docker ; \
+read -p "已添加当前用户$(whomai)到docker组,请按ctrl+c结束本次脚本,并新开ssh连接运行以使得docker组生效:"
+
 
 }
 
