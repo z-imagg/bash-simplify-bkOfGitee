c4704373ee19bd3596be036b094dd3718f09070b
prgrmz07 <prgrmz07@163.com>
2024-05-01 11:55:26 +0800

    _importBSFn: 放弃url下载

--- _importBSFn.sh
+++ _importBSFn.sh
@@ -16,13 +16,25 @@ local scriptFN=$1
 #若文件名不以'.sh'结尾, 则补上
 [[ $scriptFN = *.sh ]] || scriptFN="$scriptFN.sh"
 
-source <(curl --silent "http://giteaz:3000/bal/bash-simplify/raw/tag/${tagName}/${scriptFN}")
+#TODO 检查仓库 /app/bash-simplify/.git是否处于标签$tagName
+
+F="/app/bash-simplify/${scriptFN}"
+#若文件不存在，则返回错误
+[[ ! -f $F ]] && return 51
+
+source $F
+
+# source /app/bash-simplify/${scriptFN}
 #上一行带入实际值 后 举例如下:
-# source <(curl --silent "http://giteaz:3000/bal/bash-simplify/raw/tag/tag_release/git__chkDir__get__repoDir__arg_gitDir.sh")
+# source /app/bash-simplify/git__chkDir__get__repoDir__arg_gitDir.sh
 }
 
 #使用举例
+#1. 导入 _importBSFn.sh
 # source <(curl --silent "http://giteaz:3000/bal/bash-simplify/raw/tag/tag_release/_importBSFn.sh")
+# 或
+# source /app/bash-simplify/_importBSFn.sh
+#2. _importBSFn 导入 目标脚本文件
 # _importBSFn git__chkDir__get__repoDir__arg_gitDir
 #  等同于
 # _importBSFn git__chkDir__get__repoDir__arg_gitDir.sh
