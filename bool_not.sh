389789c4e36a8355903ad9849445198f63da1067
prgrmz07 <prgrmz07@163.com>
2024-05-04 12:06:42 +0800

    有eval的函数内局部变量必须加标识该函数的后缀

--- bool_not.sh
+++ bool_not.sh
@@ -2,8 +2,8 @@
 
 #【描述】  bool取反
 #【依赖】   
-#【术语】 
-#【备注】   
+#【术语】 _bn == _bool_not
+#【备注】   【有eval的函数内局部变量必须加标识该函数的后缀 】 所有变量名都加了后缀_bn， 理由是为了防止 eval中的变量名 即调用者函数中的变量名 和本函数变量名重复 而发生意料之外的情况
 
 
 source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/tag/tag_release/_importBSFn.sh)
@@ -17,15 +17,15 @@ function bool_not() {
 argCntEq2 $* || return $?
 
 #输入bool值
-local inBoolVar=$1
+local inBoolVar_bn=$1
 #返回 全局变量名
-local outBoolVarName=$2
+local outBoolVarName_bn=$2
 
 #取反，放入变量notBoolVar中
-local notBoolVar=false; $inBoolVar || notBoolVar=true;
+local notBoolVar_bn=false; $inBoolVar_bn || notBoolVar_bn=true;
 
 #利用eval将结果局部变量赋值给入参指定的全局变量
-eval "$outBoolVarName=$notBoolVar"
+eval "$outBoolVarName_bn=$notBoolVar_bn"
 
 }
 
--- xargsz.sh
+++ xargsz.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-declare -r ____xargsz_example_usage_txt='''【语法】xargsz 自定义bash函数名 \n【用法举例】 function busyFunc1() {  [[ "X$Ln" = 'Xbin' ]] || echo "notBinDir:$Ln" ;}  ; ls /usr/ | xargsz busyFunc1'''
-echo -e $____xargsz_example_usage_txt
+#【描述】  xargs的等效品
+#【依赖】   
+#【术语】 _xgz == _xargsz
+#【备注】   【有eval的函数内局部变量必须加标识该函数的后缀 】 所有变量名都加了后缀_xgz， 理由是为了防止 eval中的变量名 即调用者函数中的变量名 和本函数变量名重复 而发生意料之外的情况
+
+
+declare -r ____xargsz_example_usage_txt__xgz='''【语法】xargsz 自定义bash函数名 \n【用法举例】 function busyFunc1() {  [[ "X$Ln" = 'Xbin' ]] || echo "notBinDir:$Ln" ;}  ; ls /usr/ | xargsz busyFunc1'''
+echo -e $____xargsz_example_usage_txt__xgz
 
 #xargsz(等效于xargs的自定义普通bash函数)
 xargsz() {
 # set -x
 # 参数个数等于1
 if [ ! "$#" -eq 1 ]; then
-    local errCode=45
-    local errTxt="错误,函数xargsz的参数个数【$#】必须为1,退出代码【$errCode】" 
-    echo $____xargsz_example_usage_txt
-    echo $errTxt
-    return $errCode
+    local errCode_xgz=45
+    local errTxt_xgz="错误,函数xargsz的参数个数【$#】必须为1,退出代码【$errCode】" 
+    echo $____xargsz_example_usage_txt__xgz
+    echo $errTxt_xgz
+    return $errCode_xgz
 fi
 
 # echo "参数列表作为一个字符串\$*=【$*】"
 # echo "（参数列表作为字符串数组,使用请看变量arg_arr）; ${arg_arr[0]}=xxx; \$@=【$@】"
-local busyFuncName="$1"
+local busyFuncName_xgz="$1"
 
 # 循环读取输入
-while IFS= read -r line; do
+while IFS= read -r line_xgz; do
 #eval内再包裹函数的目的是为了让变量Ln以local修饰，从而名字Ln只是一个局部名字，对外层名字没有影响。
     eval """
-function __func__wrap() { local Ln=$line ; $busyFuncName ;} ;
+function __func__wrap() { local Ln=$line_xgz ; $busyFuncName_xgz ;} ;
 __func__wrap ; 
 unset __func__wrap; 
 """
