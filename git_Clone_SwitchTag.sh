63b2c89bc5f3e068fb2b8648ae3504971432eb48
prgrmz07 <prgrmz07@163.com>
2024-05-06 15:30:38 +0800

    #'-e': 任一语句异常将导致此脚本终止; '-u': 使用未声明变量将导致异常

--- git_Clone_SwitchTag.sh
+++ git_Clone_SwitchTag.sh
@@ -5,6 +5,9 @@
 #【术语】 
 #【备注】  
 
+#'-e': 任一语句异常将导致此脚本终止; '-u': 使用未声明变量将导致异常
+set -e -u
+
 source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/tag/tag_release/_importBSFn.sh)
 
 _importBSFn "git_reset.sh"
--- git__chkDir__get__repoDir__arg_gitDir.sh
+++ git__chkDir__get__repoDir__arg_gitDir.sh
@@ -5,6 +5,9 @@
 #【术语】 
 #【备注】  
 
+#'-e': 任一语句异常将导致此脚本终止; '-u': 使用未声明变量将导致异常
+set -e -u
+
 _importBSFn "argCntGe1.sh"
 
 function git__chkDir__get__repoDir__arg_gitDir() {
