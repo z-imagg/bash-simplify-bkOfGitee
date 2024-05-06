5a0b350ea50925907d391ee799f237d547ac7841
prgrmz07 <prgrmz07@163.com>
2024-05-06 11:51:43 +0800

    Url2Json.py : python解析url输出json结果

--- Url2Json.py
+++ Url2Json.py
@@ -28,16 +28,26 @@ encoded_url:str = sys.argv[1]
 Url2Json(encoded_url)
 
 #使用举例
+
 #安装依赖
 #  sudo apt install -y jq
+
 # 一般例子
 #  python /app/bash-simplify/Url2Json.py "https://github.com/xxx/yyy.html?arg1=v1&arg2=v2#p1"
 #   {"scheme": "https", "auth": null, "host": "github.com", "port": null, "path": "/xxx/yyy.html", "query": "arg1=v1&arg2=v2", "fragment": "p1"}
+
 # 解析正常例子
 #                                                               第一次解析确认是否为合法url                   若合法url 则再次解析获得结果
 #  url="https://github.com/xxx/yyy.html?arg1=v1&arg2=v2#p1" ; python /app/bash-simplify/Url2Json.py "$url" && python /app/bash-simplify/Url2Json.py "$url" | jq   .host
 #   输出 "github.com"
+
 # 解析错误例子
 #  url="::xxx" ; python /app/bash-simplify/Url2Json.py "$url" && python /app/bash-simplify/Url2Json.py "$url" | jq   .host
 #   输出 "github.com"
 
+# 日常例子
+#  url="https://github.com/xxx/yyy.html?arg1=v1&arg2=v2#p1" ; python /app/bash-simplify/Url2Json.py "$url" && \
+#  host=$(python /app/bash-simplify/Url2Json.py "$url" | jq   .host) && \
+#  去掉引号
+#  host=${host//\"/}
+#     host==github.com
