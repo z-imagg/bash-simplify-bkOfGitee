7ed3a45b8cee01081267b341ef61658b6af6e8c7
prgrmz07 <prgrmz07@163.com>
2024-05-15 10:51:40 +0800

    pyFileReFindAllDotAsAll: py正则匹配文件内容,  指定re中的点表示任意一个字符( re默认时 点表示非换行的任意一个字符 )

---
+++ pyFileReFindAllDotAsAll.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+#【描述】  py正则匹配文件内容,  指定re中的点表示任意一个字符( re默认时 点表示非换行的任意一个字符 )
+#【依赖】   
+#【术语】 
+#【备注】  
+
+
+
+import re
+reExpr=r'#define STMT.{1,20}\n  bool Traverse##CLASS.{1,70}\n#include "clang/AST/StmtNodes.inc"'
+fp="/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/include/clang/AST/RecursiveASTVisitor.h"
+fStream=open(fp)
+fTxt=fStream.read()
+ls=re.findall(reExpr, fTxt, re.DOTALL)
+[ print(k) for k in ls  ]
+
+fStream.close()
+
