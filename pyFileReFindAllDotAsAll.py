d20153d542e5c89733fbbc2893ff60da40ba3b48
prgrmz07 <prgrmz07@163.com>
2024-05-15 12:01:00 +0800

    [5] pyFileReFindAllDotAsAll

--- pyFileReFindAllDotAsAll.py
+++ pyFileReFindAllDotAsAll.py
@@ -20,40 +20,64 @@ class LnDesc:
         self.lineStartIdex:int=lineStartIdex
         self.lineLen:int=lineLen
         return
-    
+    def __repr__(self) -> str:
+        return f"si={self.lineStartIdex},L={self.lineLen}"
+
 #计算 '行下标行长度 们' LnIdxLnLen_Ls 
-def __calc__LnIdxLnLen_Ls(fTxt:str,lineFeed:str):
+def __calc__LnDescLs(fTxt:str,lineFeed:str):
     _LineFeed._assert(lineFeed)
+    _lineFeedLen:int=lineFeed.__len__()
     #行们
     lnLs=fTxt.split(lineFeed)
+    #  开发调试变量
+    _LnLen_Ls:typing.List[int]=[]
     # 行下标行长度 们
-    LnIdxLnLen_Ls:typing.List[LnDesc]=[]
+    LnDescLs:typing.List[LnDesc]=[]
     #行0 到 '行k-1' 的累计字符个数
     prevLnAccLen=0
     for lnK in lnLs:
         #第k行长度
-        lnKLen=lnK.__len__()
+        lnKLen=lnK.__len__()+_lineFeedLen
+        #  开发调试变量
+        _LnLen_Ls.append(lnKLen)
         #             行首下标 行长度
-        LnIdxLnLen_Ls.append( LnDesc( prevLnAccLen,lnKLen) )
+        LnDescLs.append( LnDesc( prevLnAccLen,lnKLen) )
         #为 '行k+1' 计算 行0 到 行k 的累计字符个数
         prevLnAccLen+=lnKLen
         
-    return LnIdxLnLen_Ls
+    return LnDescLs
 
-def __calc_lineNum__(fTxt:str,subTxt:str):
+#二分查找行号
+def __bisect_lineNum(LnDescLs:typing.List[LnDesc],subTxt_beginIdx:int)->int:
+    import bisect
+    lineStartIdex_ls:typing.List[int]=list(map(lambda x: x.lineStartIdex, LnDescLs))
+    if subTxt_beginIdx > max(lineStartIdex_ls) or subTxt_beginIdx< min(lineStartIdex_ls):
+        errMsg1=f"[1]字符下标{subTxt_beginIdx}不在有序数组lineStartIdex_ls最小值～最大值范围内"
+        raise Exception(errMsg1)
+    lineNum:int=bisect.bisect_left(lineStartIdex_ls,subTxt_beginIdx)
+    if lineNum <=0 or lineNum>=len(lineStartIdex_ls):
+        errMsg2=f"[2]字符下标{subTxt_beginIdx}不在有序数组lineStartIdex_ls最小值～最大值范围内"
+        raise Exception(errMsg2)
+    
+    return lineNum
+
+#在文本文件内容fTxt中查找子字符串subTxt的行号
+def __calc_lineNumText(fTxt:str,subTxt:str)->str:
     assert subTxt in fTxt
-    __calc__LnIdxLnLen_Ls(fTxt,"\n")
-    beginIdx:int=fTxt.index(subTxt)
+    LnDescLs:typing.List[LnDesc]=__calc__LnDescLs(fTxt,"\n")
+    subTxt_beginIdx:int=fTxt.index(subTxt)
+    beginLineNum:int=__bisect_lineNum(LnDescLs,subTxt_beginIdx)
     lineCnt:int=subTxt.count("\n")
-    pass
+    endLineNum:int=beginLineNum+lineCnt
+    return f"行{beginLineNum}～行{endLineNum}\n"
 
 def pyFileReFindAllDotAsAll(reExpr:str,txtFilePath:str):
     import re
     fStream=open(txtFilePath)
     fTxt=fStream.read()
     matchTxtLs=re.findall(reExpr, fTxt, re.DOTALL)
-    __calc_lineNum__(fTxt,matchTxtLs[0])
-    [ print(k) for k in matchTxtLs  ]
+    
+    [ print(__calc_lineNumText(fTxt,matchTxtLs[0]), k) for k in matchTxtLs  ]
 
     fStream.close()
     
