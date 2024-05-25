eca76353630e73f61a1ff7fd186171c21a63aa5b
prgrmz07 <prgrmz07@163.com>
2024-05-25 19:05:26 +0800

    [3]  gen_readme.sh 提升可读性 （ bash -c """被 alias、 #""" 代替，骗过vscode 但bash正常执行）

--- __gen_readme.md.sh
+++ __gen_readme.md.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+#'-e': 任一语句异常将导致此脚本终止; '-u': 使用未声明变量将导致异常
+set -e -u
+
+#bash允许alias展开
+shopt -s expand_aliases   
+
+# bash -c """被 alias、 #""" 代替，骗过vscode 但bash正常执行 
+alias Bgn='  xargs -0  -I@ bash    -c """  '
 
 DocF=/app/bash-simplify/readme.md
 
 find    $(pwd) -name "*.sh" -not -path  "*bash-complete-gen-from-help*" -print0   | \
-xargs -0  -I@ bash    -c """ echo -n @:  ; egrep    '^#【描述】'   @   """    | \
+Bgn true &&  echo -n @:  ; egrep    '^#【描述】'   @   #"""    | \
 python3   -c '
 import sys; 
 x=sys.stdin.read(); 
