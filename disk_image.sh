48727dca1f131c49b0dbdab69d032f69c0c25690
prgrmz07 <prgrmz07@163.com>
2024-06-07 11:27:29 +0800

    17 mkdiskimage命令包裹

--- disk_image.sh
+++ disk_image.sh
@@ -201,4 +201,6 @@ lsblk $loopX && { echo $Ok_Msg ; return $OK_ExitCode ;}
 #  调用:
 #disk_image_mk  /tmp/my_hd.img 200 16 32
 #  创建磁盘镜像文件my_hd.img  几何尺寸为 200C 16H 32S
-# 
+#disk_image__mount  /tmp/my_hd.img 32 /tmp/my_hd_dir
+#  挂载磁盘镜像到目录my_hd_dir, 挂载时 必须提供 几何参数 xxS 比如 32S==单个环型磁道有32个扇区 ,为了从xxS计算出变量Part1stByteIdx 
+#     Part1stByteIdx==1号分区在磁盘镜像文件中的下标
