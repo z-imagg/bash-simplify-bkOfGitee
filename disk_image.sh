0a2f8d008379bfc2699e602c7e3bedb6678e5ad9
prgrmz07 <prgrmz07@163.com>
2024-06-07 10:21:42 +0800

    mkdiskimage命令包裹

--- disk_image.sh
+++ disk_image.sh
@@ -12,10 +12,12 @@ source <(curl --location --silent http://giteaz:3000/bal/bash-simplify/raw/tag/t
 
 _importBSFn "ubuntu_version_check.sh"
 
+_importBSFn "arg1EqNMsg.sh"
+
 
 
 #测试mkdiskimage 是否存在及正常运行
-function _is_mkdiskimage_installed(){
+function mkdiskimage_is_installed(){
 
 #ubuntu版本检测
 ubuntu_version_check || return $?
@@ -40,3 +42,80 @@ dpkg -S syslinux-efi 2>/dev/null 1>/dev/null   && \
 #若无以上命令、包, 则执行安装
 sudo apt install -y syslinux syslinux-common syslinux-efi syslinux-utils
 }
+
+
+#2. 制作磁盘映像、注意磁盘几何参数得符合bochs要求、仅1个fat16分区
+function disk_image_mk(){
+
+local OK_ExitCode=0
+local Ok_Msg="磁盘镜像文件创建成功, 正常退出代码[$OK_ExitCode]"
+
+local Err_9=1
+local ErrMsg_9="mkdiskimage返回的Part1stByteIdx 不是预期值 $((32*512)), 请人工排查问题, 错误退出代码[$Err_9], Part1stByteIdx=$Part1stByteIdx"
+
+
+# 断言参数个数为5个
+arg1EqNMsg $# 5 '"命令用法:x.sh Url Md5sum FileName PackOutDir UnpackOutDir"' || return $?
+
+#若参数个数不为4个 ，则返回错误
+arg1EqNMsg $# 4  '[disk_image.sh][命令用法][制作磁盘映像文件] disk_image_mk HdImgF HdImg_C HdImg_H HdImg_S [命令举例][200C 16H 32S] disk_image_mk _a_hd.img 200 16 32'  || return $?
+#磁盘镜像文件路径
+local HdImgF=$1
+#磁盘镜像文件 cylind 柱面个数 (柱面==圆柱)
+local HdImg_C=$2
+#磁盘镜像文件 head 磁头个数
+local HdImg_H=$3
+#磁盘镜像文件 sectorPerTrack 每个圆环中的扇区个数 (圆环==磁道)
+local HdImg_S=$4
+
+#  Part1stByteIdx : Partition First Byte Offset : 分区的第一个字节偏移量 ： 相对于 磁盘映像文件hd.img的开头, hd.img内的唯一的分区的第一个字节偏移量
+
+#Part1stByteIdx : PartitionFirstByteOffset: 分区第一个字节在hd.img磁盘映像文件中的位置
+local Part1stByteIdx=$(mkdiskimage  -F  -o   $HdImgF $HdImg_C $HdImg_H $HdImg_S) && \
+#  当只安装syslinux而没安装syslinux-common syslinux-efi时, mkdiskimage可以制作出磁盘映像文件，但 该 磁盘映像文件  的几何尺寸参数 并不是 给定的  参数 200C 16H 32S
+#  所以 应该 同时安装了 syslinux syslinux-common syslinux-efi， "步骤1." 已有这样的检测了
+# Part1stByteIdx == $((32*512)) == 16384 == 0X4000 == 32个扇区 == SectsPerTrk个扇区 == 1个Track
+
+#测试 mkdiskimage返回的Part1stByteIdx是否为 '预期值 即 $((32*512)) 即 16384', 其中 32 是 HdImg_S
+[ $Part1stByteIdx == $((HdImg_S*512)) ] ||  { rm -v $HdImgF && echo $ErrMsg_9 && exit $Err_9 ;}
+
+echo "磁盘镜像文件创建成功 $HdImgF" && ls -lh $HdImgF && \
+{ echo $Ok_Msg; return $OK_ExitCode ;}
+
+}
+
+
+#3. 断言 磁盘映像几何参数
+#xxd -seek +0X1C3 -len 3 $HdImgF
+#0X1C3:HdImg_H -1 : 0X0F:15:即16H:即16个磁头,  0X1C4: HdImg_S : 0X20:32:即32S:即每磁道有32个扇区, 0X1C3:HdImg_C -1 : 0XC7:199:即200C:即200个柱面
+
+#0f20C7 即  用010editor打开 磁盘映像文件  偏移0X1C3到偏移0X1C3+2 的3个字节
+ 
+function disk_image__check_hdimgF_geometry_param_HSC(){
+
+local OK_ExitCode=0
+local Ok_Msg="磁盘镜像文件参数符合, 正常退出代码[$OK_ExitCode]"
+
+#若参数个数不为4个 ，则返回错误
+arg1EqNMsg $# 4  '[disk_image.sh][命令用法][制作磁盘映像文件] disk_image__check_hdimgF_geometry_param_HSC HdImgF HdImg_C HdImg_H HdImg_S [命令举例][200C 16H 32S] disk_image__check_hdimgF_geometry_param_HSC hd.img 200 16 32'  || return $?
+#磁盘镜像文件路径
+local HdImgF=$1
+#磁盘镜像文件 cylind 柱面个数 (柱面==圆柱)
+local HdImg_C=$2
+#磁盘镜像文件 head 磁头个数
+local HdImg_H=$3
+#磁盘镜像文件 sectorPerTrack 每个圆环中的扇区个数 (圆环==磁道)
+local HdImg_S=$4
+
+#测试mkdiskimage 是否存在及正常运行
+HdImg_C_sub1_hex=$( printf "%02x" $((HdImg_C-1)) ) && \
+HdImg_H_hex=$(printf "%02x" $((HdImg_H-1)) ) && \
+HdImg_S_hex=$(printf "%02x" $HdImg_S ) && \
+_HSC_hex_calc="${HdImg_H_hex}${HdImg_S_hex}${HdImg_C_sub1_hex}" && \
+_HSC_hex_xxdRdFromHdImgF="$(xxd -seek +0X1C3 -len 3  -plain  $HdImgF)" && \
+test "$_HSC_hex_xxdRdFromHdImgF" == "${_HSC_hex_calc}"  && { echo $Ok_Msg; return $OK_ExitCode ;}
+}
+
+
+
+####
