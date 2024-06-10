01b1f40fda494a1fc6a682d31c76f7058453f421
prgrmz07 <prgrmz07@163.com>
2024-06-10 09:38:03 +0800

    给定源码目录，按扩展名分组统计个数

---
+++ fileCntGroupByExtendName.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+
+#【描述】  给定源码目录，按扩展名分组统计个数
+#【依赖】   
+#【术语】 
+#【备注】  
+#【例子用法】  
+#   source /app/bash-simplify/fileCntGroupByExtendName.sh
+# 按文件扩展名分组统计目录/d2/OCCT-master中文件个数, 只关注前500个文件
+#   fileCntGroupByExtendName /d2/OCCT-master/ 500
+# 按文件扩展名分组统计目录/d2/OCCT-master中文件个数, 只关注全部文件
+#       第二个参数为0  表示关注全部文件
+#   fileCntGroupByExtendName /d2/OCCT-master/ 0
+
+
+#'-e': 任一语句异常将导致此脚本终止; '-u': 使用未声明变量将导致异常
+set -e -u
+
+function fileCntGroupByExtendName(){
+local OK=0
+local ERR=99
+
+local prj_dir=$1
+local top_n_file=$2
+
+cd $prj_dir
+
+#全部文件个数
+local all_file_cnt=$(find . -type f | wc -l)
+
+local msg1="全部文件个数为 ${all_file_cnt}"
+local msg2="只关注前 ${top_n_file} 个文件"
+
+#top_n_file为0 表示全部
+[[ $top_n_file -eq 0 ]] && top_n_file=${all_file_cnt} && msg1="关注全部文件"
+
+local msg="目录[ ${prj_dir} ] , ${msg1}, ${msg2}, 按扩展名分组统计文件个数结果如下"
+echo $msg
+
+find . -type f |head -n $top_n_file    |    xargs -I@ bash -c "echo @|rev " | xargs -I@ bash -c "echo @|cut -d'.' -f1|rev"   | sort | uniq -c | sort -nr
+
+return $OK
+}
+#函数单元测试
+#fileCntGroupByExtendName /d2/OCCT-master 500
+#fileCntGroupByExtendName /d2/OCCT-master 0
