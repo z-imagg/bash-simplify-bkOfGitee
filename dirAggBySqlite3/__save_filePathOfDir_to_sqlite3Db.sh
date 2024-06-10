99634b2665a51f1b2340e048cb007f89ffb0e438
prgrmz07 <prgrmz07@163.com>
2024-06-10 12:49:18 +0800

    再次正常插入，开发中

--- dirAggBySqlite3/__save_filePathOfDir_to_sqlite3Db.sh
+++ dirAggBySqlite3/__save_filePathOfDir_to_sqlite3Db.sh
@@ -6,7 +6,7 @@
 #【术语】 
 #【备注】  
 #【例子用法】  
-#   source /app/bash-simplify/dirAggBySqlite3/main.sh ; __save_filePathOfDir_to_sqlite3Db /d2/OCCT-master/ /tmp/sqlite3_db_filePath_OCCT-master.db
+#   source /app/bash-simplify/dirAggBySqlite3/__save_filePathOfDir_to_sqlite3Db.sh ; __save_filePathOfDir_to_sqlite3Db /d2/OCCT-master/ /tmp/sqlite3_db_filePath_OCCT-master.db
 #   fileCntGroupByExtendName /d2/OCCT-master/ 0
 
 
@@ -20,6 +20,7 @@ function __save_filePathOfDir_to_sqlite3Db(){
 local OK=0
 local ERR=99
 local fpathParsePy="/app/bash-simplify/dirAggBySqlite3/_fpathParse_forBash.py"
+local sh_echo_sqlInsertItem="/app/bash-simplify/dirAggBySqlite3/__echo_sqlInsertItem.sh"
 
 local prjDir=$1
 # local prjDir=`pwd` #调试用
@@ -37,7 +38,7 @@ local field_fExtendName="file_extend_name"
 
 local SQL_CreateTab="create table if not exists  ${tab_name} (id INTEGER PRIMARY KEY AUTOINCREMENT,  ${field_fpath} text unique, ${field_prjDir} text, ${field_parentDir} text, ${field_fname} text, ${field_fExtendName} text )  ; "
 local SQL_insert_Head="insert into ${tab_name}(   ${field_fpath}, ${field_prjDir}, ${field_parentDir}, ${field_fname}, ${field_fExtendName}) values "
-local SQL_insert_END="('END_ROW');"
+local SQL_insert_END="('END_ROW','END_ROW','END_ROW','END_ROW','END_ROW');"
 local SQL_SelectCnt="select count(*) from ${tab_name};"
 
 
@@ -45,7 +46,7 @@ echo $SQL_CreateTab | tee $SQL_tmpF 1>/dev/null
 echo $SQL_insert_Head | tee -a $SQL_tmpF 1>/dev/null
 
 #                             避免递归进入.git目录
-( cd $prjDir && find .      -path '*/.git' -prune   -or      -type f | head -n 10 | xargs -I@ bash -c "python3 $fpathParsePy  @ " | xargs -I@ bash -c "echo @; eval @; $_out4sh_fpathPy_ExecOk &&  echo ( '${prjDir}', '${_out4sh_fpath}', '${_out4sh_parentDir}', '${_out4sh_fname}', '${_out4sh_fExtendName}' ),  " | tee -a $SQL_tmpF 1>/dev/null ;)
+( cd $prjDir && find .      -path '*/.git' -prune   -or      -type f | head -n 40 | xargs -I@ bash -c "  source  <(python3 $fpathParsePy  @) ;    source $sh_echo_sqlInsertItem  " | tee -a $SQL_tmpF 1>/dev/null ;)
 #'head -n 40' 调试用
 
 # find .      -path '*/.git' -prune   -or      -type f | head -n 10 | xargs -I@ bash -c "set -x; source  <(python3 $fpathParsePy  @) ; set +x"   #调试用
@@ -53,6 +54,7 @@ echo $SQL_insert_Head | tee -a $SQL_tmpF 1>/dev/null
 echo $SQL_insert_END| tee -a $SQL_tmpF 1>/dev/null
 echo $SQL_SelectCnt| tee -a $SQL_tmpF 1>/dev/null
 # echo $SQL_tmpF; ls -lh $SQL_tmpF ; wc -l $SQL_tmpF ; head -n 5 $SQL_tmpF ; tail -n 5 $SQL_tmpF #调试用
+cat $SQL_tmpF
 # set -x #调试用
 
 [[ -e $sqlite3_db_path ]] && rm -v $sqlite3_db_path 
