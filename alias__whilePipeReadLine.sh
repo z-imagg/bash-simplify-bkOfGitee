109a4d4bcaff420d7c17c5609387a3d0d93e78e9
prgrmz07 <prgrmz07@163.com>
2024-06-25 21:15:12 +0800

    whileIFSReadEcho_alias

--- alias__whilePipeReadLine.sh
+++ alias__whilePipeReadLine.sh
@@ -7,6 +7,7 @@
 #请将以下source语句加入 ~/.bashrc 中, 再重新登陆终端, 以迫使alias生效
 #   source /app/bash-simplify/alias__whilePipeReadLine.sh
 #重新登陆终端后，可以使用本文中的alias了，  举例如下
+#   ls / | whileIFSReadEcho_alias
 #   ls / | whileIFSReadBegin_alias echo "第$k行为:$LineK"; whileIFSReadEnd_alias
 #   ls ./*.gz | whileIFSReadBeginEcho_alias [[ -f $LineK ]] && echo "  是文件"; whileIFSReadEnd_alias
 
@@ -18,3 +19,5 @@ shopt -s expand_aliases
 alias whileIFSReadBeginEcho_alias='{ k=0 &&  while IFS= read -r LineK; do true; k=$((k+1)); echo -n "[$k]=$LineK:";    '
 alias whileIFSReadBegin_alias='{ k=0 &&   while IFS= read -r LineK; do true;  k=$((k+1));    '
 alias whileIFSReadEnd_alias='  done  ;}'
+
+alias whileIFSReadEcho_alias='{ k=0 &&  while IFS= read -r LineK; do true; k=$((k+1)); echo  "[$k]=$LineK";    done  ;}'
