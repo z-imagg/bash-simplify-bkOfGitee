802a82a1e15374b2a116a012665f604e5e7d6e0d
prgrmz07 <prgrmz07@163.com>
2024-06-25 21:06:05 +0800

    简化名字

--- alias__whilePipeReadLine.sh
+++ alias__whilePipeReadLine.sh
@@ -7,8 +7,8 @@
 #请将以下source语句加入 ~/.bashrc 中, 再重新登陆终端, 以迫使alias生效
 #   source /app/bash-simplify/alias__whilePipeReadLine.sh
 #重新登陆终端后，可以使用本文中的alias了，  举例如下
-#   ls / | alias__whilePipeReadLineBegin echo "此行内容为:$LineK"; alias__whilePipeReadLineEnd
-#   ls ./*.gz | alias__whilePipeReadLineBeginEcho [[ -f $LineK ]] && echo "$LineK 是文件"; alias__whilePipeReadLineEnd
+#   ls / | whileIFSReadBegin_alias echo "此行内容为:$LineK"; whileIFSReadEnd_alias
+#   ls ./*.gz | whileIFSReadBeginEcho_alias [[ -f $LineK ]] && echo "  是文件"; whileIFSReadEnd_alias
 
 
 #'-e': 任一语句异常将导致此脚本终止; '-u': 使用未声明变量将导致异常
@@ -17,6 +17,6 @@ set -e -u
 #bash允许alias展开
 shopt -s expand_aliases
 
-alias alias__whilePipeReadLineBeginEcho=' while IFS= read -r LineK; do true; echo -n "$LineK:";    '
-alias alias__whilePipeReadLineBegin=' while IFS= read -r LineK; do true;     '
-alias alias__whilePipeReadLineEnd='  done  '
+alias whileIFSReadBeginEcho_alias=' while IFS= read -r LineK; do true; echo -n "$LineK:";    '
+alias whileIFSReadBegin_alias=' while IFS= read -r LineK; do true;     '
+alias whileIFSReadEnd_alias='  done  '
