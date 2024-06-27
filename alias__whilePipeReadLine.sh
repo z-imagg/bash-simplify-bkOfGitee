29fa42c63dab0daf6008324f7627876ceaaeb175
prgrmz07 <prgrmz07@163.com>
2024-06-27 09:35:25 +0800

    [文档调整]  用alias简化'while IFS read'，免去繁琐的记忆

--- alias__whilePipeReadLine.sh
+++ alias__whilePipeReadLine.sh
@@ -4,9 +4,10 @@
 #【依赖】   
 #【术语】 
 #【使用】
-#请将以下source语句加入 ~/.bashrc 中, 再重新登陆终端, 以迫使alias生效
-#   source /app/bash-simplify/alias__whilePipeReadLine.sh
-#重新登陆终端后，可以使用本文中的alias了，  举例如下
+#加载语句:  source /app/bash-simplify/alias__whilePipeReadLine.sh
+#1. 日常使用 （请将'加载语句'加入 ~/.bashrc 中, 再重新登陆终端, 以迫使alias生效  , 重新登陆终端后 )
+#2. 脚本调用 （任意x.sh中 引入 '加载语句' 后）
+# 可以使用本文中的alias了，  举例如下
 #   ls / | whileIFSReadEcho_alias
 #   ls / | whileIFSReadBegin_alias echo "第$k行为:$LineK"; whileIFSReadEnd_alias
 #   ls ./*.gz | whileIFSReadBeginEcho_alias [[ -f $LineK ]] && echo "  是文件"; whileIFSReadEnd_alias
