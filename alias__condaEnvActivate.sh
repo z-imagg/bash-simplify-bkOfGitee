8d8d45a21aef3cadddfeaa6b4fd13150a4be9e34
prgrmz07 <prgrmz07@163.com>
2024-07-01 10:13:11 +0800

    miniconda激活环境(使用alias)，调用方脚本语法高亮不受影响（改为alias)  [2]

--- alias__condaEnvActivate.sh
+++ alias__condaEnvActivate.sh
@@ -5,19 +5,21 @@
 #【术语】 
 #【用法举例】  
 # source /app/bash-simplify/alias__condaEnvActivate.sh
-# 以后使用 $_CondaPy 替代 python
+# 以后使用 _CondaPy_alias 替代 python 、 _CondaPip_alias 替代 pip 
 
 
 #bash允许alias展开
 shopt -s expand_aliases
 
-alias condaEnvActviate_alias=' \
-_CondaHome=/app/Miniconda3-py310_22.11.1-1 ; \
-_CondaBin=$_CondaHome/bin ; \
-_CondaActv=$_CondaBin/activate ; \
-_CondaPip=$_CondaBin/pip ; \
-_CondaPy=$_CondaBin/python ; \
-set | grep _Conda ; \
-echo "使用 \$_CondaPy 替代 python 即 相当于 激活了conda_env"
-'
+_CondaHome=/app/Miniconda3-py310_22.11.1-1
+_CondaBin=$_CondaHome/bin
+_CondaActv=$_CondaBin/activate
+alias _CondaPip_alias="$_CondaBin/pip"
+alias _CondaPy_alias="$_CondaBin/python"
+set | grep _Conda
+alias | grep _Conda
+echo "激活conda_env :
+_CondaPy_alias 替代 python 、 
+_CondaPip_alias 替代 pip 
+"
 
