8ac541114c4d5a96db77c132bdd7e8f9b8176ee3
prgrmz07 <prgrmz07@163.com>
2024-07-05 11:52:08 +0800

    [new_PrjNodejsEnv_by_nodeenv.sh]  registry.npmmirror.com

--- nodejs_script/new_PrjNodejsEnv_by_nodeenv.sh
+++ nodejs_script/new_PrjNodejsEnv_by_nodeenv.sh
@@ -97,6 +97,8 @@ cat  << EOF > $_prjNodeJsEnvActv_F
 _PATH_init="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export PNPM_HOME="$_PrjHome/.pnpm_home"
 export PATH=$_NodeBin:\$PNPM_HOME:\$_PATH_init
+export NODEJS_ORG_MIRROR=https://registry.npmmirror.com/-/binary/node
+#export NVM_NODEJS_ORG_MIRROR=https://registry.npmmirror.com/-/binary/node
 EOF
 source $_prjNodeJsEnvActv_F
 
