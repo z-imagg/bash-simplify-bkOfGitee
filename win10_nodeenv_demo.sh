2ccdfa1312f027b0765618ba7c580f7b13290b5b
prgrmz07 <prgrmz07@163.com>
2024-07-06 09:33:27 +0800

    添加 win10_nodeenv_demo.sh

---
+++ win10_nodeenv_demo.sh
@@ -0,0 +1,8 @@
+condaHome=D:\miniconda3
+set PATH=%condaHome%;%condaHome%\Scripts;%PATH%
+
+where nodeenv
+
+nodeenv  --mirror https://registry.npmmirror.com/-/binary/node --node 22.2.0  .node_envMsWin_v22.2.0
+
+
