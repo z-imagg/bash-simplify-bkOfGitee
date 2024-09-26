6b1668e1314c7d63530b33c0fd130cb354cb962f
prgrmz07 <prgrmz07@163.com>
2024-09-26 10:43:04 +0800

    [nodejs_script]   pnpm改为yarn [补充5]

--- nodejs_script/new_PrjNodejsEnv_by_nodeenv.sh
+++ nodejs_script/new_PrjNodejsEnv_by_nodeenv.sh
@@ -140,10 +140,13 @@ Npm config -g set registry $NpmMirrorTaobaoNew  #淘宝新地址
 Npm install -g yarn
 
 # yarn config set registry $NpmMirrorTaobaoOld     #淘宝旧地址，已废弃
-yarn config set registry $NpmMirrorTaobaoNew       #淘宝新地址
+yarn config set registry $NpmMirrorTaobaoNew  --global     #淘宝新地址
 # yarn config set registry https://registry.yarnpkg.com   #yarn官方原始镜像
-yarn config get registry
-yarn config list
+yarn config get registry --global  
+yarn config list --global  
+
+yarn config delete proxy --global  
+yarn config get proxy --global  
 
 #全局安装 create-vite
 Yarn global add  create-vite
