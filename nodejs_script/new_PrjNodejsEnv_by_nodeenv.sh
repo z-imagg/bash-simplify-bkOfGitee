c789cf93e9b10225e3ad7db2c7da8f8471681794
prgrmz07 <prgrmz07@163.com>
2024-09-27 11:47:58 +0800

    [nodejs_script] windows环境兼容 草稿7

--- nodejs_script/new_PrjNodejsEnv_by_nodeenv.sh
+++ nodejs_script/new_PrjNodejsEnv_by_nodeenv.sh
@@ -109,6 +109,10 @@ _PrjHome=$_PrjHome
 _NodeVer=$_NodeVer
 _NodeBin=$_NodeBin
 
+OsName=(uname --operating-system)
+msg_softlink_prj_dir="c:\Windows\System32\cmd.exe /c mklink  -Dst  $_PrjHome  -Src 当前项目目录在windows下的路径"
+[[ \$OsName=="Msys" ]] && echo \$msg_softlink_prj_dir
+
 _Err15Code=15
 _Err15Msg_newPrjEnv="错误代码 \$_Err15Code,人工执行下一行命令 以 初始化nodejs项目环境 后 再执行 此脚本PrjNodeJsEnvActivate.sh: 
 bash /app/bash-simplify/nodejs_script/new_PrjNodejsEnv_by_nodeenv.sh   \$_PrjHome    \$_NodeVer 
