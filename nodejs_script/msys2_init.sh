1971bc182a206d8dec97f2c9497d31c7ad7bd8a5
prgrmz07 <prgrmz07@163.com>
2024-09-27 13:44:43 +0800

    [nodejs_script] windows环境兼容 草稿17

--- nodejs_script/msys2_init.sh
+++ nodejs_script/msys2_init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export powersh=$(cygpath   --unix "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe")
 
 msg_softlink_prj_dir="[提醒] 微软windows下新建项目 第一步是 将项目目录 软连接到 msys2 的正确路径, 比如 cmd.exe下执行 : 'd:\bin\junction.exe   d:\msys64\<你的项目路径 比如 \app2\ncre>   <当前项目目录在windows下的路径 比如e:\ncre>'"
 
@@ -18,7 +19,7 @@ bash /d/bash-simplify/nodejs_script/download_unpack_junction.sh
 
 MsWinNoteMsg="[提醒]切换操作系统请 清空 nodejs项目环境(rm -fr  .node_env_v20.15.1 node_modules) 后 重建. 当前在微软windows操作系统下,因不同操作系统的nodejs环境目录名相同,若不清空,则相互覆盖,结果不可预料."
 
-{ echo $MsWinNoteMsg; ( cd  /app/bash-simplify/nodejs_script &&  powershell ./test-pack-install.ps1 && powershell ./msys2_link_path.ps1 ;) || ( echo $Err31Msg ; exit $Err31 ;) ;}
+{ echo $MsWinNoteMsg; ( cd  /app/bash-simplify/nodejs_script &&  $powersh ./test-pack-install.ps1 && $powersh ./msys2_link_path.ps1 ;) || ( echo $Err31Msg ; exit $Err31 ;) ;}
 
 fi
 
@@ -31,7 +32,7 @@ if [[ $isOs_Msys ]] ; then
 fullPath_nodeenv_MsWinStyle=$(cygpath   --windows  $fullPath_nodeenv)
 # 微软windows的powershell下的 '-Verb RunAs' 能从普通权限脚本中弹出UAC窗口(要求admin权限) 从而该普通权限脚本中可以有若干行以admin权限执行 
 #     -Wait 参数 迫使 该进程执行完才返回
-PowerShell -Command "Start-Process $fullPath_nodeenv_MsWinStyle -ArgumentList '--mirror $_npmmirror_taobao --node $_NodeVer $_NodejsEnvName'  -Wait -Verb RunAs"
+$powersh -Command "Start-Process $fullPath_nodeenv_MsWinStyle -ArgumentList '--mirror $_npmmirror_taobao --node $_NodeVer $_NodejsEnvName'  -Wait -Verb RunAs"
 # 软连接: bin --> Scripts
 $(cygpath --unix " d:\\bin\\junction.exe")  $(cygpath   --windows $_PrjNodeHome/bin)   $(cygpath   --windows $_PrjNodeHome/Scripts)
 
